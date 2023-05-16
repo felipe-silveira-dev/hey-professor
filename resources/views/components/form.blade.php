cddbbe93a01b263111e11e37d9e2c4465e152bfe
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2023-05-16 19:37:15 -0300

    HEY-9: refactor to use blade component

---
+++ resources/views/components/form.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'action',
+    'post' => null,
+    'put' => null,
+    'delete' => null,
+])
+
+<form action="{{ $action }}" method="post">
+    @csrf
+    @if ($put)
+        @method('put')
+    @endif
+    @if ($delete)
+        @method('delete')
+    @endif
+    {{ $slot }}
+</form>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,4 @@
+@props(['title'])
+<h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+    {{ $title }}
+</h2>
