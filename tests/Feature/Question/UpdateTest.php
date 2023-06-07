05352051cc167f5f80f5a1e98b8fb5184e90ca23
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2023-06-07 16:53:23 -0300

    HEY-22: it should make sure that question owner and question with status draft can update

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\User;
+use App\Models\{Question, User};
 
 use function Pest\Laravel\actingAs;
 
@@ -18,4 +18,45 @@ it('should be able to update a question', function () {
         ->assertRedirect();
 
     expect($question->fresh()->question)->toBe('This is a updated question?');
+});
+
+it('should make sure that only questions with status DRAFT can be update', function () {
+    $user             = User::factory()->create();
+    $questionNotDraft = Question::factory()
+                    ->for($user, 'createdBy')
+                    ->create(['draft' => false]);
+    $questionDraft = Question::factory()
+    ->for($user, 'createdBy')
+    ->create(['draft' => true]);
+
+    actingAs($user)
+        ->put(route('question.update', $questionNotDraft))
+        ->assertForbidden();
+
+    actingAs($user)
+        ->put(route('question.update', $questionDraft), [
+            'question' => 'This is a updated question?',
+        ])
+        ->assertRedirect();
+    expect($questionDraft->fresh()->question)->toBe('This is a updated question?');
+});
+
+it('should make sure that question owner can update', function () {
+    $user          = User::factory()->create();
+    $questionOwner = Question::factory()
+                    ->for($user, 'createdBy')
+                    ->create(['draft' => true]);
+    $questionNotOwner = Question::factory()->create(['draft' => true]);
+
+    actingAs($user)
+        ->put(route('question.update', $questionOwner), [
+            'question' => 'This is a updated question?',
+        ])
+        ->assertRedirect();
+
+    expect($questionOwner->fresh()->question)->toBe('This is a updated question?');
+
+    actingAs($user)
+        ->put(route('question.update', $questionNotOwner))
+        ->assertForbidden();
 });
