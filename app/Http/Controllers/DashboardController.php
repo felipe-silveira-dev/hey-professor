2b6250eaef9ecdc91c8c4620c1a8c790497b7aa6
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2023-06-07 21:25:48 -0300

    HEY-24: should order by like and unlike, most liked question should be at the top, most unliked questions should be in the bottom

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,15 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
-    public function __invoke(): \Illuminate\Contracts\View\View
+    // public function __invoke(): \Illuminate\Contracts\View\View
+    // {
+    //     return view('dashboard', [
+    //         'questions' => Question::withSum('votes', 'like')
+    //                 ->withSum('votes', 'unlike')
+    //                 // if(votes_sum_like is null, 0, votes_sum_like) desc,
+    //                 // if(votes_sum_unlike is null, 0, votes_sum_unlike)
+    //                 ->orderByRaw('case when votes_sum_like is null then 0 else 1 end desc, case when votes_sum_unlike is null then 0 else 1 end')
+    //                 ->paginate(5),
+    //     ]);
+
+    public function __invoke(): View
     {
         return view('dashboard', [
-            'questions' => Question::withSum('votes', 'like')
-                    ->withSum('votes', 'unlike')
-                    ->paginate(5),
+            'questions' => Question::query()
+                ->withSum('votes', 'like')
+                ->withSum('votes', 'unlike')
+                ->orderByRaw('
+                    case when votes_sum_like is null then 0 else votes_sum_like end desc,
+                    case when votes_sum_unlike is null then 0 else votes_sum_unlike end
+                ')
+                ->paginate(5),
         ]);
     }
 }
--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,5 +1,5 @@
 @if ($paginator->hasPages())
-    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between dark">
+    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between">
         <div class="flex justify-between flex-1 sm:hidden">
             @if ($paginator->onFirstPage())
                 <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 dark:hover:text-gray-400 dark:hover:border-gray-500 dark:hover:bg-gray-700 dark:hover:bg-opacity-10 dark:focus:bg-gray-700 dark:focus:bg-opacity-10 dark:focus:text-gray-400 dark:focus:border-gray-500 dark:active:bg-gray-500 dark:active:bg-opacity-10">
@@ -44,8 +44,7 @@
                     {{-- Previous Page Link --}}
                     @if ($paginator->onFirstPage())
                         <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
-                            <span class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default rounded-l-md leading-5 
-                            dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 dark:hover:text-gray-400 dark:hover:border-gray-500 dark:hover:bg-gray-700 dark:hover:bg-opacity-10 dark:focus:bg-gray-700 dark:focus:bg-opacity-10 dark:focus:text-gray-400 dark:focus:border-gray-500 dark:active:bg-gray-500 dark:active:bg-opacity-10" aria-hidden="true">
+                            <span class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default rounded-l-md leading-5 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 dark:hover:text-gray-400 dark:hover:border-gray-500 dark:hover:bg-gray-700 dark:hover:bg-opacity-10 dark:focus:bg-gray-700 dark:focus:bg-opacity-10 dark:focus:text-gray-400 dark:focus:border-gray-500 dark:active:bg-gray-500 dark:active:bg-opacity-10" aria-hidden="true">
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                                 </svg>
