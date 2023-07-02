5710173ae89690cd9fa631734338fee80507b7fb
felipe-silveira-dev <felipe.silveira@eduzz.com.br>
2023-07-02 19:29:02 -0300

    HEY-29: filter backend

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,24 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
 
 class DashboardController extends Controller
 {
-    // public function __invoke(): \Illuminate\Contracts\View\View
-    // {
-    //     return view('dashboard', [
-    //         'questions' => Question::withSum('votes', 'like')
-    //                 ->withSum('votes', 'unlike')
-    //                 // if(votes_sum_like is null, 0, votes_sum_like) desc,
-    //                 // if(votes_sum_unlike is null, 0, votes_sum_unlike)
-    //                 ->orderByRaw('case when votes_sum_like is null then 0 else 1 end desc, case when votes_sum_unlike is null then 0 else 1 end')
-    //                 ->paginate(5),
-    //     ]);
-
     public function __invoke(): View
     {
         return view('dashboard', [
             'questions' => Question::query()
+                ->when(request()->has('search'), function (Builder $query) {
+                    $query->where('question', 'like', '%' . request()->search . '%');
+                })
                 ->withSum('votes', 'like')
                 ->withSum('votes', 'unlike')
                 ->orderByRaw('
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
             @foreach ($questions as $item)
                 <x-question :question="$item"/>
             @endforeach
-            {{ $questions->links() }}
+            {{ $questions->withQueryString()->links() }}
         </div>
     </x-container>
 </x-app-layout>
