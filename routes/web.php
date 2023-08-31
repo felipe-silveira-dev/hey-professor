bd950adf4a21d54f9659a3b649dff558240cd5aa
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-08-31 16:35:31 -0300

    HEY-33: sign in with github

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     if (app()->isLocal()) {
-        auth()->loginUsingId(1);
+        // auth()->loginUsingId(1);
 
-        return to_route('dashboard');
+        // return to_route('dashboard');
     }
 
     return view('welcome');
 });
 
 Route::get('/github/login', Github\RedirectController::class)->name('github.login');
+Route::get('/github/callback', Github\CallbackController::class)->name('github.callback');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
