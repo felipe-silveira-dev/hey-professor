5e6bfba2c0ff525e25702105df653f809d7e0026
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-09-10 21:12:43 -0300

    HEY-347: using livewire

--- app/Livewire/UploadForm.php
+++ app/Livewire/UploadForm.php
@@ -2,11 +2,43 @@
 
 namespace App\Livewire;
 
+use App\Models\Media;
+use App\Rules\UserUploadValidator;
+use App\Services\UploadService;
 use Illuminate\Contracts\View\View;
-use Livewire\Component;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+use Livewire\{Component, WithFileUploads};
 
-class UploadForm extends Component
+final class UploadForm extends Component
 {
+    use WithFileUploads;
+
+    public null|string|TemporaryUploadedFile $file = null;
+
+    public function rules(): mixed
+    {
+        return (new UserUploadValidator())->avatars();
+    }
+
+    public function upload(UploadService $service): void
+    {
+        $this->validate();
+
+        try {
+            $file = $service->avatar(
+                $this->file,
+            );
+        } catch (\Throwable $e) {
+            throw $e;
+        }
+
+        // fazer que o phpstan ignore o erro
+        /** @phpstan-ignore-next-line */
+        Media::query()->create([
+            $file->toArray(),
+        ]);
+    }
+
     public function render(): View
     {
         return view('livewire.upload-form');
--- resources/views/livewire/upload-form.blade.php
+++ resources/views/livewire/upload-form.blade.php
@@ -1,3 +1,35 @@
 <div>
-    {{-- Be like water. --}}
+    <form wire:submit.prevent="upload" class="flex flex-col space-y-4">
+        <input type="file" wire:model="file" class="border border-gray-300 rounded-md p-2">
+        <button type="submit" class="bg-blue-500 text-white rounded-md p-2">
+            Upload
+        </button>
+    </form>
+
+    @if ($file)
+        <div class="flex flex-col space-y-4">
+            <img src="{{ $file->temporaryUrl() }}" alt="" class="w-64 h-64 object-cover">
+
+            <div class="flex flex-col space-y-2">
+                <span class="font-bold">Path</span>
+                <span>{{ $file->getRealPath() }}</span>
+            </div>
+            <div class="flex flex-col space-y-2">
+                <span class="font-bold">Name</span>
+                <span>{{ $file->getClientOriginalName() }}</span>
+            </div>
+            <div class="flex flex-col space-y-2">
+                <span class="font-bold">Size</span>
+                <span>{{ $file->getSize() }}</span>
+            </div>
+            <div class="flex flex-col space-y-2">
+                <span class="font-bold">Extension</span>
+                <span>{{ $file->getClientOriginalExtension() }}</span>
+            </div>
+            <div class="flex flex-col space-y-2">
+                <span class="font-bold">Mime Type</span>
+                <span>{{ $file->getMimeType() }}</span>
+            </div>
+        </div>
+    @endif
 </div>
